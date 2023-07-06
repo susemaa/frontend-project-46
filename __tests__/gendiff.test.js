e44106461536ddb049b49f71f734e27aec77e47d
Arseny Radoslavov <rad.archeg@gmail.com>
2023-07-06 13:26:51 +0300

    added test for deep trees

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -12,14 +12,14 @@ test('correct flow json', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
   const expectedString = readFile('expected.txt');
-
   expect(gendiff(filepath1, filepath2)).toEqual(expectedString);
-});
 
-test('correct flow yaml', () => {
-  const filepath1 = getFixturePath('file1.yml');
-  const filepath2 = getFixturePath('file2.yaml');
-  const expectedString = readFile('expected.txt');
+  const ymlFilepath1 = getFixturePath('file1.yml');
+  const ymlFilepath2 = getFixturePath('file2.yaml');
+  expect(gendiff(ymlFilepath1, ymlFilepath2)).toEqual(expectedString);
 
-  expect(gendiff(filepath1, filepath2)).toEqual(expectedString);
+  const deepFilepath1 = getFixturePath('deep1.json');
+  const deepFilepath2 = getFixturePath('deep2.json');
+  const deepExpectedString = readFile('expected_deep.txt');
+  expect(gendiff(deepFilepath1, deepFilepath2)).toEqual(deepExpectedString);
 });
