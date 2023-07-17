820bf5cc7aacd1b8297be486f03ceae3a81f82ad
Arseny Radoslavov <rad.archeg@gmail.com>
2023-07-17 15:05:39 +0300

    fixed tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,7 +1,7 @@
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import fs from 'fs';
-import gendiff from '../src/utils.js';
+import gendiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
